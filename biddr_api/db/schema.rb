0cb4128df64ea72ec9bd5740490f2fd6b93a34be
Sabithapol <Sabitha.poladi10@gmail.com>
2020-09-16 15:27:18 -0700

    commit

--- biddr_api/db/schema.rb
+++ biddr_api/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_202136) do
+ActiveRecord::Schema.define(version: 2020_09_16_212855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "auctions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
