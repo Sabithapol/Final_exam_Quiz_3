e8e17def7b98a283d11ef4219681e0e2c03e941c
Sabithapol <Sabitha.poladi10@gmail.com>
2020-09-16 13:34:22 -0700

    version and scaffold

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :bids
+  resources :sessions
+  resources :users
+  resources :auctions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
