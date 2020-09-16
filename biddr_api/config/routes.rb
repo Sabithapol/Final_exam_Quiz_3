73d3ba26be21888d801fd4b99c0dfae5ef40baa6
Sabithapol <Sabitha.poladi10@gmail.com>
2020-09-16 13:36:34 -0700

    added namespace in routes

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :bids
-  resources :sessions
-  resources :users
-  resources :auctions
+  namespace :api do
+    namespace :api do
+      resources :bids
+      resources :sessions
+      resources :users
+      resources :auctions
+    end
+  end
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
